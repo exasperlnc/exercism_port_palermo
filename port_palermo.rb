89fd0672922701a45dc5de2a3fdba26787dada74
Logan Cole <emtlogancole@gmail.com>
2024-04-15 11:27:58 -0500

    passed

--- port_palermo.rb
+++ port_palermo.rb
@@ -3,11 +3,11 @@ module Port
 
   def self.get_identifier(city)
     # edge case city name is less than 4
-    city[0-3].to_sym
+    city[0..3].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    if ship_identifier.to_s =~ "OIL" || ship_identifier.to_s =~ "GAS"
+    if ship_identifier.to_s =~ /OIL/ || ship_identifier.to_s =~ /GAS/
       return :A 
     else
       return :B 
