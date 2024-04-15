0f866819da64d3c1c224c4429859d943686b08f4
Logan Cole <emtlogancole@gmail.com>
2024-04-15 11:21:53 -0500

    Initial Commit

---
+++ port_palermo.rb
@@ -0,0 +1,16 @@
+module Port
+  Port::IDENTIFIER = :PALE  
+
+  def self.get_identifier(city)
+    # edge case city name is less than 4
+    city[0-3].to_sym
+  end
+
+  def self.get_terminal(ship_identifier)
+    if ship_identifier.to_s =~ "OIL" || ship_identifier.to_s =~ "GAS"
+      return :A 
+    else
+      return :B 
+    end
+  end
+end
